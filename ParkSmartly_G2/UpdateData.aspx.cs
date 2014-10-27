f412edaedfe35fa611c795f2cc475fb8f2f5f7c2
sandip <reddivarisandeep@gmail.com>
2014-10-27 11:41:30 -0400

    data saving to database
    coded connection and single value saving function in update data page.

--- ParkSmartly_G2/UpdateData.aspx.cs
+++ ParkSmartly_G2/UpdateData.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -13,7 +15,18 @@ public partial class UpdateData : System.Web.UI.Page
     }
     protected void Add_Click(object sender, EventArgs e)
     {
+        try
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Park_smartly_conStr"].ConnectionString);
+            con.Open();
+            string cmndstr_psd = "Insert Into ParkingInformation (Floor,space) values ('" + Tbox_floNo.Text + "','" + Tbox_SpaceNo.Text + "')";
+            SqlCommand insert = new SqlCommand(cmndstr_psd, con);
+            insert.ExecuteNonQuery();
+            con.Close();
 
+        }
+        catch { Lbl_err.Visible = true; }
+        Response.Redirect("UpdateData.aspx");
     }
     protected void Add_mul_Click(object sender, EventArgs e)
     {
