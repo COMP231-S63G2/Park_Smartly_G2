043add069c809831f4e9b98d28795e531822e741
Shahbaaz <shahbaaz1491@gmail.com>
2014-11-19 00:03:57 -0500

    Created UI for make payment.
    Added Make payment button and redirected to paypal site#58

---
+++ ParkSmartly_G2/Confirmation.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="Confirmation.aspx.cs" Inherits="Confirmation" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <div class="t1" style="height: 550px; width: 800px; float: left; margin-left: 10px; margin-top: 10px;">
+
+
+
+
+
+
+
+
+
+        <div class="form-group" style="margin-top: 10px; margin-left: 130px">
+
+                                <div class="col-sm-12" style="padding-left: 200px; margin-top: 20px;">
+                                    <asp:Button ID="Btn_Payment" class="btn btn-default" runat="server" Text="Confirm & Make Payment" OnClick="Btn_Payment_Click" Width="180px" />
+                                </div>
+                            </div>
+    </div>
+</asp:Content>
+
---
+++ ParkSmartly_G2/Confirmation.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Confirmation : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void Btn_Payment_Click(object sender, EventArgs e)
+    {
+          decimal amount = 100;
+         string RegistrationId = "1234234" ;
+        string siteName = "Parksmartly";
+         string redirect = "";
+        redirect += "https://www.paypal.com/xclick/business=parksmartly@gmail.com";
+        redirect += "&item_name=" + siteName + " Order " + RegistrationId;
+        redirect += "&item_number=" + RegistrationId;
+        redirect += "&amount=" + String.Format("{0:0.00} ", amount);
+        redirect += "&currency=USD";
+        redirect += "&return=http://localhost.";
+        redirect += "&cancel_return=http://localhost.";
+        Response.Redirect(redirect);
+    }
+}
