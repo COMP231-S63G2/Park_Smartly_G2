0dceee0b76959877c9ef5d84188d72b024f2127b
dp-comp231s63g2 <darshrp@yahoo.com>
2014-11-27 02:23:03 -0500

    Implemented Logic For Extend parking-time limit.
    I have implemented Logic for time extend option in time extending page.
    #78

--- ParkSmartly_G2/ExtendTime.aspx.cs
+++ ParkSmartly_G2/ExtendTime.aspx.cs
@@ -11,67 +11,92 @@ public partial class ExtendTime : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        Lbl_err_msg.Text = "";
     }
     protected void Btn_extd_Click(object sender, EventArgs e)
     {
-        string date, id, spc, flr, l_plt, hrs, amt, E_tm = null;
+        string date, id, spc, flr, l_plt, hrs, amt, E_tm = null, ex_amt;
+        int txt_hrs;
+
 
         try
         {
-
-            if (!String.IsNullOrEmpty(Tb_noPlt.Text))
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Park_smartly_conStr"].ConnectionString);
+            con.Open();
+            string cmndstr_id = "Select id from ParkingInformation where Number_Plate ='" + Tb_noPlt.Text + "'";
+            SqlCommand getid = new SqlCommand(cmndstr_id, con);
+            string gid = getid.ExecuteScalar().ToString();
+            con.Close();
+            if (!String.IsNullOrEmpty(gid))
             {
-
-                SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Park_smartly_conStr"].ConnectionString);
-                conn.Open();
-                string cmndstr_getDtls = "Select * from ParkingInformation where Number_Plate = '" + Tb_noPlt.Text + "'";
-                SqlCommand getData = new SqlCommand(cmndstr_getDtls, conn);
-                SqlDataReader dr = getData.ExecuteReader();
-
-                while (dr.Read())
+                if (!String.IsNullOrEmpty(Tb_noPlt.Text))
                 {
 
+                    SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Park_smartly_conStr"].ConnectionString);
+                    conn.Open();
+                    string cmndstr_getDtls = "Select * from ParkingInformation where Number_Plate = '" + Tb_noPlt.Text + "'";
+                    SqlCommand getData = new SqlCommand(cmndstr_getDtls, conn);
+                    SqlDataReader dr = getData.ExecuteReader();
 
-                    id = dr["id"].ToString();
-                    spc = dr["space"].ToString();
-                    flr = dr["Floor"].ToString();
-                    date = dr["Date"].ToString();
-                    l_plt = dr["Number_Plate"].ToString();
-                    hrs = dr["Hours"].ToString();
-                    E_tm = dr["ExitTime"].ToString();
-                    amt = dr["Amount"].ToString();
+                    while (dr.Read())
+                    {
 
+
+                        id = dr["id"].ToString();
+                        spc = dr["space"].ToString();
+                        flr = dr["Floor"].ToString();
+                        date = dr["Date"].ToString();
+                        l_plt = dr["Number_Plate"].ToString();
+                        hrs = dr["Hours"].ToString();
+                        E_tm = dr["ExitTime"].ToString();
+                        amt = dr["Amount"].ToString();
+
+                    }
+                    dr.Close();
+                    conn.Close();
                 }
-                dr.Close();
-                conn.Close();
-            }
-            else if (!String.IsNullOrEmpty(Tb_id.Text))
-            {
-                SqlConnection cnn = new SqlConnection(ConfigurationManager.ConnectionStrings["Park_smartly_conStr"].ConnectionString);
-                cnn.Open();
-                string cmndstr_getDtls = "Select * from ParkingInformation where id = '" + Tb_id.Text + "'";
-                SqlCommand getData = new SqlCommand(cmndstr_getDtls, cnn);
-                SqlDataReader dr = getData.ExecuteReader();
-
-                while (dr.Read())
+                else if (!String.IsNullOrEmpty(Tb_id.Text))
                 {
+                    SqlConnection cnn = new SqlConnection(ConfigurationManager.ConnectionStrings["Park_smartly_conStr"].ConnectionString);
+                    cnn.Open();
+                    string cmndstr_getDtls = "Select * from ParkingInformation where id = '" + Tb_id.Text + "'";
+                    SqlCommand getData = new SqlCommand(cmndstr_getDtls, cnn);
+                    SqlDataReader dr = getData.ExecuteReader();
+
+                    while (dr.Read())
+                    {
 
 
-                    id = dr["id"].ToString();
-                    spc = dr["space"].ToString();
-                    flr = dr["Floor"].ToString();
-                    date = dr["Date"].ToString();
-                    l_plt = dr["Number_Plate"].ToString();
-                    hrs = dr["Hours"].ToString();
-                    E_tm = dr["ExitTime"].ToString();
-                    amt = dr["Amount"].ToString();
+                        id = dr["id"].ToString();
+                        spc = dr["space"].ToString();
+                        flr = dr["Floor"].ToString();
+                        date = dr["Date"].ToString();
+                        l_plt = dr["Number_Plate"].ToString();
+                        hrs = dr["Hours"].ToString();
+                        E_tm = dr["ExitTime"].ToString();
+                        amt = dr["Amount"].ToString();
+                    }
+                    dr.Close();
+                    cnn.Close();
+
                 }
-                dr.Close();
-                cnn.Close();
+                else { Lbl_err_msg.Text = "Licence Plate OR Ticket Id must be provided"; Lbl_err_msg.Visible = true; }
 
             }
-            else { Lbl_err_msg.Text = "Licence Plate OR Ticket Id must be provided"; Lbl_err_msg.Visible = true; }
+            else { Lbl_err_msg.Text = "Data Not Matching, Try Again"; }
+
+
+            txt_hrs = Int32.Parse(Tb_hrs.Text);
+
+            if (txt_hrs != null)
+            {
+
+
+                DateTime ext_time = Convert.ToDateTime(E_tm);
+                DateTime nw_ext_tm = ext_time.AddHours(txt_hrs);
+                ex_amt = (txt_hrs * 4).ToString();
+
+            }
         }
         catch (NullReferenceException)
         {
