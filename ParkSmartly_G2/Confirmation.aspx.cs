d5c365dd440995997e7501d116876e398e01bd8f
Sandeep <reddivarisandeep@gmail.com>
2014-11-29 20:33:43 -0500

    done Get registration data from database task
    Completed Get registration data from database task of confirmation.

--- ParkSmartly_G2/Confirmation.aspx.cs
+++ ParkSmartly_G2/Confirmation.aspx.cs
@@ -64,6 +64,30 @@ public partial class Confirmation : System.Web.UI.Page
 
     protected void Btn_confirm_Click(object sender, EventArgs e)
     {
-       
+        SqlConnection cnn = new SqlConnection(ConfigurationManager.ConnectionStrings["Park_smartly_conStr"].ConnectionString);
+        cnn.Open();
+        string cmndstr_getDtls = "Select * from Registration_Data where reference_id = '" + id + "'";
+        SqlCommand getData = new SqlCommand(cmndstr_getDtls, cnn);
+        SqlDataReader dr = getData.ExecuteReader();
+
+        string mtm, atm, ntm, name, e_id, c_no, dt, amt, r_id;
+        while (dr.Read())
+        {
+
+
+            mtm = dr["mor_time"].ToString();
+            atm = dr["aft_time"].ToString();
+            ntm = dr["nig_time"].ToString();
+            name = dr["name"].ToString();
+            e_id = dr["email_id"].ToString();
+            c_no = dr["contact_no"].ToString();
+            dt = dr["date"].ToString();
+            amt = dr["amount"].ToString();
+            r_id = dr["reference_id"].ToString();
+
+        }
+        dr.Close();
+        cnn.Close();
+
     }
 }
