262d993a9396410407b97cd6e1b987628138c269
Shahbaaz <shahbaaz1491@gmail.com>
2014-12-01 23:59:01 -0500

    verified input data
    Verified input data with database

--- ParkSmartly_G2/SearchLocation.aspx.cs
+++ ParkSmartly_G2/SearchLocation.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -13,6 +15,28 @@ public partial class SearchLocation : System.Web.UI.Page
     }
     protected void Btn_search_Click(object sender, EventArgs e)
     {
+          static string date, id, spc, flr, l_plt, hrs, amt, E_tm = null;
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Park_smartly_conStr"].ConnectionString);
+        conn.Open();
+        string cmndstr_getDtls = "Select * from ParkingInformation where Number_Plate = '" + Tb_noPlt.Text + "'";
+        SqlCommand getData = new SqlCommand(cmndstr_getDtls, conn);
+        SqlDataReader dr = getData.ExecuteReader();
 
+        while (dr.Read())
+        {
+
+
+            id = dr["id"].ToString();
+            spc = dr["space"].ToString();
+            flr = dr["Floor"].ToString();
+            date = dr["Date"].ToString();
+            l_plt = dr["Number_Plate"].ToString();
+            hrs = dr["Hours"].ToString();
+            E_tm = dr["ExitTime"].ToString();
+            amt = dr["Amount"].ToString();
+
+        }
+        dr.Close();
+        conn.Close();
     }
 }
