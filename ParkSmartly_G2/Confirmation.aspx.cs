41f1a09c1cc133d666f45b9df236c024b06a91df
Shahbaaz <shahbaaz1491@gmail.com>
2014-12-03 14:23:23 -0500

    Implemented business logic of email sending
    Implemented business logic of email sending. #93

--- ParkSmartly_G2/Confirmation.aspx.cs
+++ ParkSmartly_G2/Confirmation.aspx.cs
@@ -64,30 +64,72 @@ public partial class Confirmation : System.Web.UI.Page
 
     protected void Btn_confirm_Click(object sender, EventArgs e)
     {
-        SqlConnection cnn = new SqlConnection(ConfigurationManager.ConnectionStrings["Park_smartly_conStr"].ConnectionString);
-        cnn.Open();
-        string cmndstr_getDtls = "Select * from Registration_Data where reference_id = '" + id + "'";
-        SqlCommand getData = new SqlCommand(cmndstr_getDtls, cnn);
-        SqlDataReader dr = getData.ExecuteReader();
+        SqlConnection cnctn = new SqlConnection(ConfigurationManager.ConnectionStrings["Park_smartly_conStr"].ConnectionString);
+        cnctn.Open();
 
-        string mtm, atm, ntm, name, e_id, c_no, dt, amt, r_id;
-        while (dr.Read())
+        string cmstr = "Insert into Registration_Data (mor_time,aft_time,nig_time,allocated,name,email_id,contact_no,date,amount,reference_id) values (@mor_time,@aft_time,@nig_time,@allocated,@name,@email_id,@contact_no,@date,@amount,@reference_id) ";
+
+        SqlCommand insert_regi_data = new SqlCommand(cmstr, cnctn);
+        insert_regi_data.Parameters.AddWithValue("@mor_time", mor_time);
+        insert_regi_data.Parameters.AddWithValue("@aft_time", aft_time);
+        insert_regi_data.Parameters.AddWithValue("@nig_time", nig_tm);
+        insert_regi_data.Parameters.AddWithValue("@allocated", 1);
+        insert_regi_data.Parameters.AddWithValue("@name", name);
+        insert_regi_data.Parameters.AddWithValue("@email_id", ema_id);
+        insert_regi_data.Parameters.AddWithValue("@contact_no", co_no);
+        insert_regi_data.Parameters.AddWithValue("@date", date);
+        insert_regi_data.Parameters.AddWithValue("@amount", amnt);
+        insert_regi_data.Parameters.AddWithValue("@reference_id", r_id);
+
+
+        try
+        {
+            insert_regi_data.ExecuteNonQuery();
+            cnctn.Close();
+
+        }
+        catch (NullReferenceException ex) { }
+
+        try
         {
 
 
-            mtm = dr["mor_time"].ToString();
-            atm = dr["aft_time"].ToString();
-            ntm = dr["nig_time"].ToString();
-            name = dr["name"].ToString();
-            e_id = dr["email_id"].ToString();
-            c_no = dr["contact_no"].ToString();
-            dt = dr["date"].ToString();
-            amt = dr["amount"].ToString();
-            r_id = dr["reference_id"].ToString();
+            string body = string.Empty;
+            using (StreamReader reader = new StreamReader(Server.MapPath("~/EmailTemplate.html")))
+            {
+                body = reader.ReadToEnd();
+            }
+            body = body.Replace("{Name}", name);
+            body = body.Replace("{Rnum}", r_id);
+            body = body.Replace("{Date}", date);
+            body = body.Replace("{Amt}", amnt);
+
+
+
+
+            MailMessage mail = new MailMessage();
+            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+            mail.From = new MailAddress("parksmartly2@gmail.com");
+            mail.To.Add("gkgaurav74@gmail.com");
+            mail.Subject = "Online Parking Confirmatiion";
+
+            mail.IsBodyHtml = true;
+            mail.Body = body;
+
+            SmtpServer.Port = 587;
+            SmtpServer.Credentials = new System.Net.NetworkCredential("parksmartly2@gmail.com", "smartlyparkg2");
+            SmtpServer.EnableSsl = true;
+
+            SmtpServer.Send(mail);
+            // MessageBox.Show("mail Send");
+                     
 
         }
-        dr.Close();
-        cnn.Close();
+        catch (Exception ex)
+        {
+            //  MessageBox.Show(ex.ToString());
+        }
 
     }
 }
