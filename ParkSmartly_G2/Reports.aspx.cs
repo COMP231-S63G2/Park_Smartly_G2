9da4cec7a7b678a622d28c33d79a51e5628f73f8
dp-comp231s63g2 <darshrp@yahoo.com>
2014-12-06 12:57:50 -0500

    Completed Get list of data task
    I have filled the dropdown list with list from database.#97

--- ParkSmartly_G2/Reports.aspx.cs
+++ ParkSmartly_G2/Reports.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +12,25 @@ public partial class Reports : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        bindDepartments();
+    }
+    protected void bindDepartments()
+    {
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["Park_smartly_conStr"].ConnectionString);
+        cn.Open();
+        string ADDStr = "Select Distinct Month([Date]) from dbo.ParkingInformation";
+        SqlCommand ADDCmd = new SqlCommand(ADDStr, cn);
+        DataTable table = new DataTable();
+
+
+        SqlDataAdapter adapter = new SqlDataAdapter(ADDCmd);
+
+        adapter.Fill(table);
+
+        DropDownList1.DataSource = table;
+        DropDownList1.DataValueField = "Month([Date]"; //The Value of the DropDownList, to get it you should call DropDownList1.SelectedValue;
+        DropDownList1.DataTextField = "Month([Date]"; //The Name shown of the DropDownList.
+        DropDownList1.DataBind();
 
     }
 }
